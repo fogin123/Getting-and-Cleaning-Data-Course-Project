23f61546df74b6412a5e1b076f3c5c70271d2d95
Arthur Akhmetov <fogin@mail.ru>
2017-07-23 00:33:55 +0500

    added final version of files

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # Getting and Cleaning Data Course Project
 
-###### The purpose of this project is to collect and clean data from the studies of wearable computing.
+###### Getting and clean data from data sets of accelerometer and gyroscope studies
 
-_This will also be italic_
 
-**This text will be bold**
-__This will also be bold__
+###### The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
 
-_You **can** combine them_
+###### The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain. See 'features_info.txt' for more details. 
 
-* Item 1
-* Item 2
-  * Item 2a
-  * Item 2b
+
+R-script with one function "GetAndCleanSets" doing next:
+* 1. Collect all nessesary data in variable "data"
+* 2. Edit and renaming it
+* 3. Make "tidy_data" massive to consolidate global data and writing it in "tidy_data.txt" file 
+* 4. Return tidy_data
+
+Incoming variable for function "directory" is character vector that describing global directory of studies files. File "tidy_data.txt" is making in the same directory.
+
+Originally task for this r-script was:
+
+*Merges the training and the test sets to create one data set.
+*Extracts only the measurements on the mean and standard deviation for each measurement.
+*Uses descriptive activity names to name the activities in the data set
+*Appropriately labels the data set with descriptive variable names.
+*From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The repository consist of the next files:
+* README.md
+* Codebook.md - describes the variables, the data, and transformations that was performed to clean up the data
+* run_analysis.R - basic r-script for collecting data.
--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,34 @@
+# Getting and clean data from data sets of accelerometer and gyroscope studies
+
+# initializing library "dplyr" for working with data sets
 library(dplyr)
+
+# writing function "GetAndCleanSets" doing next:
+# 1. Collect all nessesary data in variable "data"
+# 2. Edit and renaming it
+# 3. Make "tidy_data" massive to consolidate global data and writing it in "tidy_data.txt" file 
+# 4. Return tidy_data
+# Incoming variable for function "directory" is character vector that describing global directory of studies files
 
 GetAndCleanSets <- function (directory)
 {
+  #getting information of activitites to convert it as factors later
   activities <- read.table(paste(directory,"/activity_labels.txt",sep=""),as.is = TRUE)
   activities <- unlist(activities$V2)
   
+  #getting information of labels for global studies
   features <- read.table(paste(directory,"/features.txt",sep=""),as.is = TRUE)
   features <- unlist(features$V2)
+  #make unique names for labels for correct work of "select" function
   features <- make.unique(features)
   
+  #getting data from "test" directory
   test_data <- read.table(paste(directory,"/test/X_test.txt",sep=""))
   names(test_data) <- features
+  #selecting only mean and std variables
   test_data <- select(test_data,matches("-(mean|std)\\(\\)\\-"))
   
+  #adding corresponding information about subjects and activities for primary data
   subj_test <- read.table(paste(directory,"/test/subject_test.txt",sep=""))
   test_data["subject"] <- NA
   test_data["subject"] <- subj_test$V1
@@ -21,6 +37,7 @@ GetAndCleanSets <- function (directory)
   test_data["activity"] <- NA
   test_data["activity"] <- activ_test$V1
   
+  #doing same thing for "train" directory
   train_data <- read.table(paste(directory,"/train/X_train.txt",sep=""))
   names(train_data) <- features
   train_data <- select(train_data,matches("-(mean|std)\\(\\)\\-"))
@@ -33,10 +50,24 @@ GetAndCleanSets <- function (directory)
   train_data["activity"] <- NA
   train_data["activity"] <- activ_train$V1
   
+  #binding global data from "test" and "train" to "data" variable
   data <- rbind(train_data,test_data)
   
+  #converting information about activities from indexes to factors
   data$activity <- factor(data$activity)
   levels(data$activity) <- activities
   
-  data
+  #correcting names of "data" variable - changing them to lower index, change character "-" to "_", deleting "()"
+  names(data) <- tolower(names(data))
+  names(data) <- gsub("-","_",names(data))
+  names(data) <- gsub("\\(\\)","",names(data))
+  
+  #making tidy data with the average of each variable for each activity and each subject
+  tidy_data <- group_by(data,subject,activity)
+  tidy_data <- tidy_data %>% summarise_all(funs(mean))
+  
+  #write tidy data into file "tidy_data.txt" in global directory
+  write.table(tidy_data,paste(fileURL,"/tidy_data.txt",sep=""),row.names = FALSE)
+  
+  tidy_data
 }
